89134f6eb352c4008a88005d373356de61a5162b
Antonio Roldan Andrade <antonioroldanandrade@gmail.com>
2022-05-15 21:24:22 +0200

    subida P3 v2 :poop:

--- Roldan_Andrade_Antonio/P3/pedido-articulo.d
+++
@@ -1,3 +0,0 @@
-pedido-articulo.o pedido-articulo.d : pedido-articulo.cpp pedido-articulo.hpp articulo.hpp \
-  ../P1/cadena.hpp ../P1/fecha.hpp pedido.hpp usuario.hpp tarjeta.hpp \
-  usuario-pedido.hpp
